55d56c4d1ecdaf87b2ce72377343babc7be6a4e6
RkReddy <rkreddii01@gmail.com>
2023-09-04 05:09:09 +0000

    validation Tests file added

---
+++ tests_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"testing"
+)
+
+func TestSetOperation(t *testing.T) {
+	payload := map[string]string{
+		"key":   "test-key",
+		"value": "test-value",
+	}
+	payloadBytes, _ := json.Marshal(payload)
+
+	resp, err := http.Post("http://localhost:8000/set", "application/json", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		t.Fatalf("Failed to make POST request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code 200, but got %d", resp.StatusCode)
+	}
+
+	// You can add further assertions if needed
+}
+
+func TestGetOperation(t *testing.T) {
+	key := "abc-1"
+
+	resp, err := http.Get("http://localhost:8000/get/" + key)
+	if err != nil {
+		t.Fatalf("Failed to make GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code 200, but got %d", resp.StatusCode)
+	}
+
+	// You can add further assertions if needed
+}
+
+func TestSearchOperation(t *testing.T) {
+	queryParam := "abc"
+
+	resp, err := http.Get("http://localhost:8000/search?prefix=" + queryParam)
+	if err != nil {
+		t.Fatalf("Failed to make GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code 200, but got %d", resp.StatusCode)
+	}
+
+}
